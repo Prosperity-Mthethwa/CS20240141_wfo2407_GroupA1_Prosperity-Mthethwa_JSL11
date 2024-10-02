065d36875f109bf8677ef8ab70b3fa3eefdcb0cb
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-10-02 11:11:01 +0200

    imported initialData and taskFunctions

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // TASK: import helper functions from utils
+import {getTasks,createNewTask,patchTask,putTask,deleteTask} from "./utils/taskFunctions.js";
+
 // TASK: import initialData
+
+import { initialData } from "./initialData.js"
 
 
 /*************************************************************************************************************************************************
